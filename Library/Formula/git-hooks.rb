1fe7a6e404c80ee4f18a60a22c03cfccd09e25a2
Chris Blackburn <chris@midwiretech.com>
2015-01-26 14:59:21 -0600

    Add a caveat

--- Library/Formula/git-hooks.rb
+++ Library/Formula/git-hooks.rb
@@ -8,6 +8,19 @@ class GitHooks < Formula
     bin.install "git-hooks"
   end
 
+  def caveats
+    <<-EOS.undent
+      Run 'git hooks --install' Inside a git project to change that projects
+      git hooks to use git-hooks hooks. 'git hooks --uninstall' can be run at
+      any time to go back to the old hooks that were installed before
+      (typically the default which does nothing).
+
+      Run 'git hooks --installglobal' to force any new git repository or any
+      git repository you clone to have a reminders to install git hooks. (It
+      can't be on by default for security reasons of course)
+    EOS
+  end
+
   test do
     output = `cd $(brew --prefix); git hooks`.strip
     assert_match /Listing User, Project, and Global hooks/, output
